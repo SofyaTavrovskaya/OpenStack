c77a59c9e52fb65150501f03d159c9b3090b046a
SofyaTavrovskaya <tavrovskayasofya87@gmail.com>
2018-08-07 15:27:48 +0300

    openstack

---
+++ cleanup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+virsh net-destroy external
+virsh net-destroy internal
+virsh net-destroy management
+
+virsh net-undefine external
+virsh net-undefine internal
+virsh net-undefine management
+
+virsh destroy vm1
+virsh destroy vm2
+
+virsh undefine vm1
+virsh undefine vm2
+
+
+
